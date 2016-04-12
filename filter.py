2a280b18d3f1ccd78b635a9ca9db77cdd92ec010
Arsh Chauhan <achauhan@alaska.edu>
2016-04-12 13:20:56 -0800

    Filter employees based on department

--- filter.py
+++ filter.py
@@ -3,6 +3,7 @@
 """
 	Arsh Chauhan
 	04/11/2016
+	Edited: 04/12/2016
 	filter.py: Removes empty entires from a JSON file
 	Made for a UAF Cyber Security Club exercise 
 """
@@ -10,25 +11,26 @@
 import json
 import time
 
-"""
-	removeInvalid: Remove invalid employee ID'Security
-	Pre: None
-	Post: Returns a lst with all empty entries removed. 
-"""
-def removeInvalid(data,length):
-	validEmployees = []
-	for index in range(0,length):
-		if data[index]:
-			validEmployees.append(data[index])
-	return validEmployees
+def search(employees,department):
+	ITEmployees = []
+	for person in employees:
+		if person["4Department"] == department:
+			ITEmployees.append(person)
+	return ITEmployees
 
 
 if __name__ == '__main__':
 	startTime  =  time.time()
 	with open("employees.json") as jsonFile:
 		rawDatabase = json.load(jsonFile)
-	validEmployees = removeInvalid(rawDatabase,len(rawDatabase))
-	employeeFile = open('validEmployees.json','w')
-	json.dump(validEmployees,employeeFile)
+	ITEmployees = search(rawDatabase,"Information Technology")
+	Management = search (rawDatabase,"Management")
+	HREmployees = search(rawDatabase,"Human Resources")
+	IT = open('informationTechnology.json','w')
+	Mng = open("management.json",'w')
+	HR = open('humanResources.json','w')
+	json.dump(ITEmployees,IT)
+	json.dump(Management,Mng)
+	json.dump(HREmployees,HR)
 	endTime = time.time()
 	print("Run Time: ") + str(endTime - startTime)
